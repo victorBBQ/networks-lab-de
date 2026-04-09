1521c9b266c20786913b790b2f994b7a41693fda
Victor Antonio Valenzuela <v.valenzuela@edu.bbq.de>
2026-04-09 10:21:30 +0200

    fix: update astro config with site and base for gh-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://victorBBQ.github.io',
+  base: '/networks-lab',
   vite: {
     plugins: [tailwindcss()],
   },
