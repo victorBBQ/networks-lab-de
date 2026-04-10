c31255e5d36f39786bbc9243a1698d37e26374ed
Victor Antonio Valenzuela <v.valenzuela@edu.bbq.de>
2026-04-10 08:51:24 +0200

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://victorBBQ.github.io',
-  base: '/networks-lab',
+  base: '/networks-lab-de',
   vite: {
     plugins: [tailwindcss()],
   },
